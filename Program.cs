2377e009356f0d990ccac3bf89c79bcba7a7a9fe
kholio-hub <km7604007@mail.com>
2026-03-06 00:59:55 +0200

    cat task

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+Console.WriteLine("enter number of subjects");
+int sub =Convert.ToInt32(Console.ReadLine());
+int[] arr= new int[sub];
+Console.WriteLine("enter your grades:");
+for(int i=0;i<sub;i++)
+{
+    arr[i]=Convert.ToInt32(Console.ReadLine());
+}
+
+double avg =calculateavg (arr);
+Console.WriteLine("Average = " + avg);
+if (avg >= 85)
+    
+            Console.WriteLine("Grade: Excellent");
+        else if (avg >= 75)
+            Console.WriteLine("Grade: Very Good");
+        else if (avg >= 65)
+            Console.WriteLine("Grade: Good");
+        else if (avg >= 50)
+            Console.WriteLine("Grade: Pass");
+        else
+            Console.WriteLine("Grade: Fail");
+    
+    }
+
+
+
+
+
+ static double calculateavg(int[] arr)
+{
+    int sum=0;
+    foreach(int grade in arr)
+    sum+=grade;
+    return (double)sum / arr.Length;
+}
+}
